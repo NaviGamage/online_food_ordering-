e65550f7965900abdb4da4a135c3b982c82f236c
NaviGamage <navinduprabashwara@gmail.com.com>
2025-11-02 10:01:02 +0530

    create the CustomerDto.java

---
+++ src/main/java/edu/icet/model/dto/CustomerDto.java
@@ -0,0 +1,21 @@
+package edu.icet.model.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class CustomerDto {
+
+        private String userid;
+        private String name;
+        private String email;
+        private String phone;
+        private String address;
+   }
+
+
