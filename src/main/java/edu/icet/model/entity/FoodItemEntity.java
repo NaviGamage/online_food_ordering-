035cf4e5ed4265397a0a31577a170ffde6195479
NaviGamage <navinduprabashwara@gmail.com.com>
2025-11-02 11:36:19 +0530

    create the FoodItemEntity

---
+++ src/main/java/edu/icet/model/entity/FoodItemEntity.java
@@ -0,0 +1,25 @@
+package edu.icet.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+
+public class FoodItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean available;
+}
+
